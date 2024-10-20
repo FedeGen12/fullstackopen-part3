9985749ae722ed6e0232118d146ec75702f80e68
Federico Genaro <fedegen14@gmail.com>
2024-10-20 03:40:40 -0300

    Exercise 3.7 - Morgan

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 const baseUrl = '/api/persons'
 const MAX_USERS = 121212
